5625e398ff97eaa3169ffd78c2ef76ce714ae13b
jxxh204 <ahhancom@gmail.com>
2025-02-03 21:23:14 +0900

    chore : strictmode 제거

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -6,10 +6,8 @@ import theme from './styles/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  // <React.StrictMode>
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     <App />
   </ThemeProvider>
-  // </React.StrictMode>
 )
