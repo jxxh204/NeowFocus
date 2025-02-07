61370ebeeaa398adb6eb938acb789d1c1018e632
jxxh204 <ahhancom@gmail.com>
2025-02-07 19:54:00 +0900

    style : 포커스페이지 > 윈도우 최소화 버튼 퍼블리싱

--- src/renderer/src/page/focus/index.tsx
+++ src/renderer/src/page/focus/index.tsx
@@ -2,6 +2,23 @@ import { useEffect } from 'react'
 import useWindowSize from '@renderer/hooks/useWindowSize'
 import pawDarkGray from '@assets/paw_dark_gray.svg'
 import TextField from '@renderer/component/TextField'
+import windowMinimize from '@assets/window_minimize.svg'
+import styled from 'styled-components'
+
+const FocusPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  height: 100%;
+`
+
+const WindowMinimizeIcon = styled.img`
+  width: 22px;
+  height: 22px;
+  cursor: pointer;
+  margin-bottom: 6px;
+`
 
 export function FocusPage(): JSX.Element {
   const { setWindowSize } = useWindowSize()
@@ -19,5 +36,14 @@ export function FocusPage(): JSX.Element {
   }, [])
 
   //제거하기.
-  return <TextField placeholder="태스크 이름 연동 필요" stretch disabled imgSrc={pawDarkGray} />
+  return (
+    <FocusPageWrapper>
+      <WindowMinimizeIcon src={windowMinimize} alt="윈도우 최소화 아이콘" />
+      <TextField placeholder="태스크 이름 연동 필요" stretch disabled imgSrc={pawDarkGray} />
+      <div>
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </FocusPageWrapper>
+  )
 }
