f8dea4c9322a851e4c6435c55b82f52e266f6c97
jxxh204 <ahhancom@gmail.com>
2025-02-18 22:33:01 +0900

    feat : PopupProvider 구현 후 ask,complete 팝업 적용

---
+++ src/renderer/src/context/PopupContext.tsx
@@ -0,0 +1,42 @@
+import AskPopup from '@renderer/page/focus/components/Popup/AskPopup'
+import CompletedPopup from '@renderer/page/focus/components/Popup/CompletedPopup'
+import { createContext, useContext, useState } from 'react'
+
+type PopupProps = {
+  children: React.ReactNode
+}
+type PopupType = 'ask' | 'completed'
+
+const PopupContext = createContext({
+  isPopupOpen: false,
+  openPopup: (popup: PopupType) => {},
+  closePopup: () => {}
+})
+
+export const PopupProvider = ({ children }: PopupProps) => {
+  const [popup, setPopup] = useState<PopupType>()
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
+  const openPopup = (popup: PopupType) => {
+    setPopup(popup)
+    setIsPopupOpen(true)
+  }
+  const closePopup = () => {
+    setIsPopupOpen(false)
+    setPopup(undefined)
+  }
+  return (
+    <PopupContext.Provider value={{ isPopupOpen, openPopup, closePopup }}>
+      {children}
+      {isPopupOpen && popup === 'ask' && <AskPopup />}
+      {isPopupOpen && popup === 'completed' && <CompletedPopup />}
+    </PopupContext.Provider>
+  )
+}
+
+export const usePopup = () => {
+  const context = useContext(PopupContext)
+  if (!context) {
+    throw new Error('usePopup must be used within a PopupProvider')
+  }
+  return context
+}
--- src/renderer/src/page/focus/components/Completed.tsx
+++ src/renderer/src/page/focus/components/Completed.tsx
@@ -2,12 +2,16 @@ import Button from '@renderer/component/Button'
 import { ButtonWrapper } from '../styled'
 import { EndWrap } from './styled'
 import PawCircleWhiteSvg from '@assets/paw_circle_white.svg'
-
+import { usePopup } from '@renderer/context/PopupContext'
 const Completed = () => {
+  const { openPopup } = usePopup()
+  const handleClickEnd = () => {
+    openPopup('completed')
+  }
   return (
     <>
       <ButtonWrapper>
-        <Button value="이 작업 끝!" type="outlined" />
+        <Button value="이 작업 끝!" type="outlined" onClick={handleClickEnd} />
         <Button value="한번 더" type="filled" />
       </ButtonWrapper>
 
